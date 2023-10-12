ee04a14cc1e71a25ea4a15fde07799e721dcbaa0
Valtteri Lakaniemi <valtteri.lakaniemi@gmail.com>
2023-10-13 01:13:50 +0300

    Allow CORS for local frontend

--- backend-python-fastapi/app/main.py
+++ backend-python-fastapi/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .routes import todo_list_router, todo_router
 
@@ -6,6 +7,17 @@ app = FastAPI(title="TODO App Backend", version="1.0.0")
 
 app.include_router(todo_list_router)
 app.include_router(todo_router)
+
+# TODO: should probably make this configurable somewhere
+allowed_origins = ["http://localhost:5173"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=allowed_origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/", include_in_schema=False)
